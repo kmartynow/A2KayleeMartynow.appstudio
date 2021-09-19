ac888c7179bd7dd0938bafeef37f43a3b1ed0362
Kaylee Martynow <kayleemartynow@creighton.edu>
2021-09-19 13:28:59 -0500

    After day 1

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,8 @@
+
+let user = "Kaylee"
+let stateName = ["NE","FL"]
+userState = prompt ("Enter a state using a capitalized two letter abreviation:")
+// if userState = 
+// let outsideTemp = outsideTemp = Prompt ("Enter the temperature as a number of degree farenheit:")
+
+
